24d153670b647360eca60af0b75e572553e7a15c
PremanshBehl <premanshbehl978@gmail.com>
2026-05-01 14:25:41 +0530

    feat(backend): implement utilities and error handling middleware

---
+++ finance-tracker-backend/src/middleware/errorMiddleware.js
@@ -0,0 +1,30 @@
+import AppError from '../utils/appError.js';
+import { config } from '../config/env.js';
+
+export const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (config.nodeEnv === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  } else {
+    // Production
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    } else {
+      console.error('ERROR 💥', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went very wrong!',
+      });
+    }
+  }
+};
