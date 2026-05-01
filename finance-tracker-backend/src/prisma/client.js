9c1f55af22cc4206668ca5fe789f38063a6f8447
PremanshBehl <premanshbehl978@gmail.com>
2026-05-01 14:25:41 +0530

    feat(backend): setup database connection and schema

---
+++ finance-tracker-backend/src/prisma/client.js
@@ -0,0 +1,14 @@
+import prismaPkg from '@prisma/client';
+const { PrismaClient } = prismaPkg;
+import { PrismaPg } from '@prisma/adapter-pg';
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
